7f99b3387260bfa80afbb0302fe234cc45c3f807
mawi <42104518+DEM0NAssissan7@users.noreply.github.com>
2021-05-30 08:17:36 -0500

    Add the permissions setter
    This allows for people who pull directly off the github page to be able to set the permissions very easily.

---
+++ set-permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+chmod +x createThumbnail.js
+chmod +x ding.js
+chmod +x export-zip.sh
+chmod +x kill.py
+chmod +x local_install.sh
+chmod +x po
+chmod +x schemas
+echo Permissions have been set!
